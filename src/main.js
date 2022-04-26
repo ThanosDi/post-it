e535fc0f3c2d0c224f2d33912132393a3dbbe30c
ThanosDi <thanosdi@live.com>
2022-04-26 12:23:08 +0300

    Add menu with text align, last edited date and word count

--- src/main.js
+++ src/main.js
@@ -1,78 +1,76 @@
 // Modules to control application life and create native browser window
-const {app, BrowserWindow, screen} = require('electron');
-const path = require('path');
-const contextMenu = require('electron-context-menu');
-const Store = require('electron-store');
+const { app, BrowserWindow, screen } = require("electron");
+const path = require("path");
+const contextMenu = require("electron-context-menu");
+const Store = require("electron-store");
 
 try {
-	require('electron-reloader')(module);
+  require("electron-reloader")(module);
 } catch {}
 
 const store = new Store();
-
-// Add close item to the context menu
-contextMenu({
-	append: () => [
-		{
-			label: 'Close Post-it',
-			visible: true,
-			click: () => {
-				app.quit();
-			}
-		}
-	]
-});
-
+const width = store.get("postit-width") || 500;
+const height = store.get("postit-height") || 500;
 // Create the window to hold the note.
 function createWindow() {
-	const display = screen.getPrimaryDisplay();
-	const screenWidth = display.bounds.width;
-	const screenHeight = display.bounds.height;
-	const width = 500;
-	const height = 400;
-	const x = screenWidth - width;
-	const y = screenHeight / 2 - height / 2;
-	const mainWindow = new BrowserWindow({
-		width,
-		height,
-		x: store.get('posted-position-x') || x,
-		y: store.get('posted-position-y') || y,
-		acceptFirstMouse: true,
-		autoHideMenuBar: true,
-		resizable: true,
-		transparent: true,
-		frame: false,
-		movable: true,
-		hasShadow: false,
-		alwaysOnTop: true,
-		vibrancy: 'ultra-dark',
-		webPreferences: {
-			nodeIntegration: true,
-			enableRemoteModule: true
-		}
-	});
+  const display = screen.getPrimaryDisplay();
+  const screenWidth = display.bounds.width;
+  const screenHeight = display.bounds.height;
+  const x = screenWidth - width;
+  const y = screenHeight / 2 - height / 2;
+  const mainWindow = new BrowserWindow({
+    width,
+    height,
+    x: store.get("posted-position-x") || x,
+    y: store.get("posted-position-y") || y,
+    acceptFirstMouse: true,
+    autoHideMenuBar: false,
+    resizable: true,
+    transparent: true,
+    frame: false,
+    movable: true,
+    hasShadow: false,
+    alwaysOnTop: true,
+    vibrancy: "ultra-dark",
+    webPreferences: {
+      nodeIntegration: true,
+      enableRemoteModule: true,
+    },
+  });
 
-	// and load the index.html of the app.
-	mainWindow.loadFile(path.join(__dirname, '/index.html'));
+  // and load the index.html of the app.
+  mainWindow.loadFile(path.join(__dirname, "/index.html"));
 
-	// Disable maximize
-	mainWindow.on('maximize', () => {
-		mainWindow.unmaximize();
-	});
+  // Disable maximize
+  mainWindow.on("maximize", () => {
+    mainWindow.unmaximize();
+  });
 
-	// Save last app position before close.
-	mainWindow.on('close', () => {
-		const [x, y] = mainWindow.getPosition();
-		store.set('posted-position-x', x);
-		store.set('posted-position-y', y);
-	});
+  // Save last app position before close.
+  mainWindow.on("close", () => {
+    const [x, y] = mainWindow.getPosition();
+    store.set("posted-position-x", x);
+    store.set("posted-position-y", y);
+  });
+  // Add close item to the context menu
+  contextMenu({
+    append: () => [
+      {
+        label: "Close Post-it",
+        visible: true,
+        click: () => {
+          app.quit();
+        },
+      },
+    ],
+  });
 }
 
-app.whenReady().then(() => setTimeout(createWindow, 100));
+app.whenReady().then(() => setTimeout(createWindow, 10));
 
 // Quit when all windows are closed, except on macOS. There, it's common
 // for applications and their menu bar to stay active until the user quits
 // explicitly with Cmd + Q.
-app.on('window-all-closed', () => {
-	if (process.platform !== 'darwin') app.quit();
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") app.quit();
 });
--- src/renderer.js
+++ src/renderer.js
@@ -1,13 +1,62 @@
-// Save textarea text on key up and restore on render.
-const Store = require('electron-store');
+const debounce = require("debounce");
+const moment = require("moment-timezone");
+const Store = require("electron-store");
 
+/* constants */
+const timezone = Intl.DateTimeFormat().resolvedOptions().timeZone;
+moment.tz.setDefault(timezone);
+
+/* Init Store */
 const store = new Store();
+const savedText = store.get("postit") || "";
+const postitAlign = store.get("postit-align") || "center";
+const lastEditedDate =
+  store.get("postit-lastEdited") || moment().format("YYYY-MM-DD hh:mm:ss");
 
-const saveText = event => {
-	store.set('postit', event.target.value);
+/* HTML Elements */
+const postit = document.querySelector("textarea");
+const wordCount = document.querySelector("#word-count");
+const alignLeft = document.querySelector("#align-left");
+const alignCenter = document.querySelector("#align-center");
+const alignRight = document.querySelector("#align-right");
+const lastEdited = document.querySelector("#last-edited span");
+
+const setLastEdited = () => {
+  const now = moment().format("YYYY-MM-DD hh:mm:ss");
+  lastEdited.textContent = now;
+  store.set("postit-lastEdited", now);
 };
 
-const postit = document.querySelector('textarea');
-postit.addEventListener('keyup', saveText);
-const savedText = store.get('postit') || '';
+const setWordCount = count => {
+  wordCount.textContent = count;
+};
+
+/* Event Listeners */
+alignLeft.addEventListener("click", () => {
+  postit.style.textAlign = "left";
+  store.set("postit-align", "left");
+});
+alignCenter.addEventListener("click", () => {
+  postit.style.textAlign = "center";
+  store.set("postit-align", "center");
+});
+alignRight.addEventListener("click", () => {
+  postit.style.textAlign = "right";
+  store.set("postit-align", "right");
+});
+postit.addEventListener("keyup", event => {
+  store.set("postit", event.target.value);
+  setWordCount(postit.value.length);
+});
+window.onresize = debounce(() => {
+  store.set("postit-width", window.innerWidth);
+  store.set("postit-height", window.innerHeight);
+}, 200);
+
+postit.onkeydown = debounce(setLastEdited, 400);
+
+/* Load saved values on render */
+lastEdited.textContent = lastEditedDate;
 postit.value = savedText;
+postit.style.textAlign = postitAlign;
+setWordCount(postit.value.length);
